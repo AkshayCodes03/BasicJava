de39bf599c08ce932b6510529a30124a39527090
akshay <akshay@localhost>
2021-02-27 17:17:50 -0600

    Do while loop will print numbers 1 to 10

---
+++ BasicJava/src/FlowControl/DoWhileLoop.java
@@ -0,0 +1,16 @@
+package FlowControl;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int num = 10;
+		int i = 1;
+		do {
+			System.out.print(i);
+			i++;
+		}while(i <= num);
+	}
+
+}
