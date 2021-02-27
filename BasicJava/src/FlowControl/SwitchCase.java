03c186d3810f184d1f1bae9de1c01b9c53788d24
akshay <akshay@localhost>
2021-02-27 17:02:08 -0600

    Switch Program

--- BasicJava/src/FlowControl/SwitchCase.java
+++ BasicJava/src/FlowControl/SwitchCase.java
@@ -2,8 +2,23 @@ package FlowControl;
 
 public class SwitchCase {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		char c = 'd';
+		
+		switch(c)
+		{
+			case 'a':
+				System.out.println("Not the correct character");
+				break;
+			case 'q':
+				System.out.println(c);
+				break;
+			default:
+				System.out.println("Not the correct character");
+				break;
+		}
 
 	}
 
