f292a43fc81ccda7511bd94106a33e9dfe5c8a3c
akshay <akshay@localhost>
2021-02-27 17:12:36 -0600

    While loop program will print values 1 to 10.

---
+++ BasicJava/src/FlowControl/WhileLoop.java
@@ -0,0 +1,16 @@
+package FlowControl;
+
+public class WhileLoop 
+{
+	public static void main(String args[])
+	{
+		int num = 10;
+		int i = 1;
+		while(i <= num)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
