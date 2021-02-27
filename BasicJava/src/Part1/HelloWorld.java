309735256fe1bdd2a12304b0dce8e966d1e6447c
akshay <akshay@localhost>
2021-02-27 14:36:10 -0600

    Run Hello World Successful

--- BasicJava/src/Part1/HelloWorld.java
+++ BasicJava/src/Part1/HelloWorld.java
@@ -2,8 +2,10 @@ package Part1;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		System.out.print("Hello world");
 
 	}
 
